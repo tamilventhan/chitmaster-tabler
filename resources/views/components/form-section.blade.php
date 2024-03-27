320cceb83a091eb98282956627f411310c9eb985
Tamilventhan T <78096762+tamilventhan@users.noreply.github.com>
2024-03-27 21:11:38 +0530

    aligned profile

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,22 +1,26 @@
 @props(['submit'])
 
 <div {{ $attributes->merge(['class' => '']) }}>
-    <x-section-title>
-        <x-slot name="title">{{ $title }}</x-slot>
-        <x-slot name="description">{{ $description }}</x-slot>
-    </x-section-title>
+    <div class="d-lg-flex justify-content-between">
+        <x-section-title class="col-md-5">
+            <x-slot name="title">{{ $title }}</x-slot>
+            <x-slot name="description">{{ $description }}</x-slot>
+        </x-section-title>
 
-    <div class="card">
-        <form wire:submit="{{ $submit }}">
-            <div class="card-body">
-                {{ $form }}
-            </div>
+        <div class="col-md-7">
+            <div class="card">
+                <form wire:submit="{{ $submit }}">
+                    <div class="card-body">
+                        {{ $form }}
+                    </div>
 
-            @if (isset($actions))
-            <div class="card-footer">
-                {{ $actions }}
+                    @if (isset($actions))
+                    <div class="card-footer">
+                        {{ $actions }}
+                    </div>
+                    @endif
+                </form>
             </div>
-            @endif
-        </form>
+        </div>
     </div>
 </div>
