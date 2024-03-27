dae61ef3bccc05842f00ceadb2e59af915213eb1
Tamilventhan T <78096762+tamilventhan@users.noreply.github.com>
2024-03-28 00:00:37 +0530

    fixed forget password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,45 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+@extends('tablar::auth.layout')
+@section('content')
+@section('title', 'Forgot Password')
+<div class="container py-5">
+    <div class="col-md-6 offset-md-3">
+        <x-authentication-card>
+            <x-slot name="logo">
+                {{--
+                <x-authentication-card-logo /> --}}
+            </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a
+                password reset link that will allow you to choose a new one.') }}
+            </div>
 
-        @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            @session('status')
+            <div class="mb-4 font-medium text-sm text-green">
                 {{ $value }}
             </div>
-        @endsession
+            @endsession
 
-        <x-validation-errors class="mb-4" />
+            <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
 
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" type="email" name="email" :value="old('email')"
+                        required autofocus autocomplete="username" />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+                <div class="mt-4 d-lg-flex justify-content-lg-between">
+                    <x-button>
+                        {{ __('Email Password Reset Link') }}
+                    </x-button>
+                    <a href="{{ route('login') }}">back to Login</a>
+                    
+                </div>
+            </form>
+        </x-authentication-card>
+    </div>
+</div>
+@endsection
--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -2,7 +2,7 @@
     <div {{ $attributes }}>
         <div class="invalid-feedback">{{ __('Whoops! Something went wrong.') }}</div>
 
-        <ul class="fee">
+        <ul class="fee text-red">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
