3138c6d8fb268cf7c01d67c4e2414f42173dcc7f
Tamilventhan T <78096762+tamilventhan@users.noreply.github.com>
2024-03-28 22:59:30 +0530

    added books fevicon

--- resources/views/vendor/tablar/master.blade.php
+++ resources/views/vendor/tablar/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/books.png') }}">
 
     {{-- Custom Meta Tags --}}
     @yield('meta_tags')
