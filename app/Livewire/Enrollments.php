f56b9dc72424a2ee41223205675d3c63ff7a564c
Tamilventhan T <78096762+tamilventhan@users.noreply.github.com>
2024-06-01 17:46:37 +0530

    added enrollement, wire:navigate

---
+++ app/Livewire/Enrollments.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Enrollment;
+use Livewire\WithPagination;
+
+class Enrollments extends Component
+{
+    
+    use WithPagination;
+    
+    public $sortField = 'id';
+    public $sortAsc = true;
+    public $entries = 12;
+
+    
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortAsc = !$this->sortAsc;
+        } else {
+            $this->sortAsc = true;
+        }
+
+        $this->sortField = $field;
+    }
+
+    public function render()
+    {
+        $enrollments = Enrollment::orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')->latest()->paginate($this->entries);
+
+        return view('livewire.enrollments',compact('enrollments'))->layout('tablar::page');
+    }
+}
--- resources/views/vendor/tablar/partials/navbar/single-item.blade.php
+++ resources/views/vendor/tablar/partials/navbar/single-item.blade.php
@@ -1,4 +1,4 @@
-<a class="dropdown-item" href="{{ $item['href']??'' }}">
+<a wire:navigate class="dropdown-item" href="{{ $item['href']??'' }}">
     @if(isset($item['icon']))
         <span class="nav-link-icon d-md-none d-lg-inline-block">
                         <!-- Download SVG icon from http://tabler-icons.io/i/package -->
